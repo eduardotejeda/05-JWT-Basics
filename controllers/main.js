dbbb9c89074a89276940a884598fe3b73542f133
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-11-28 23:37:38 -0400

    Add custom errors and middleware

--- controllers/main.js
+++ controllers/main.js
@@ -3,6 +3,7 @@ const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
 const {username, password} = req.body
+console.log(req.body)
 
 //mongoose
 //Joi
@@ -15,37 +16,26 @@ throw new CustomAPIError('Please provide username and password', 400)
 // just for demo, normally privided by DB!
 const id = new Date().getDate
 
+
 //try to keep payload small, better experience for user
 const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn:'30d'})
 
-res.status(200).json({msg:'user created', token})
 
-console.log(username, password);
-  res.send('Fake Login/Register/Signup Route')
+res.status(200).json({msg:'user created', token})
 }
 
 const dashboard = async (req, res) => {
-const authHeader = req.headers.authorization;
 
-if(!authHeader || !authHeader.startsWith('')) {
-  throw new CustomAPIError('No token provided', 401)
-}
+  console.log(req.user)
 
-const token = authHeader.split(' ')[0]
-console.log(token)
-
-try {
-  const decoded = jwt.verify(token, process.env.JWT_SECRET)
-  console.log(decoded)
-  
-} catch (error) {
-  throw new CustomAPIError('Not autorized to access this route', 401)
-}
+  const luckyNumber = Math.floor(Math.random()*100)
+  res.status(200).json({
+    msg:`Hello, ${req.user.username}`, 
+    secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
 
 
 
-const luckyNumber = Math.floor(Math.random()*100)
-res.status(200).json({msg:`Hello, JohnDoe`, secret:`Here is your authorized data, your lucky number is  ${luckyNumber}`})
+
 }
 
 module.exports = {
